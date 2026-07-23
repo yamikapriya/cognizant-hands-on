4401e6f2c258ff9bac85ba9854e20b9baabb885a
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 17:36:09 +0530

    Create BookRepository.java

---
+++ week2/spring core/dependency injections/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void displayBook() {
+        System.out.println("Book Repository: Book data retrieved successfully.");
+    }
+}
