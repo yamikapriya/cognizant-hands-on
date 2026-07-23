058e4db3ddd35d2b83692dce26da5890cdc48b6e
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 15:06:24 +0530

    Create Product.java

---
+++ Week 1/Algorithms_Data Structures/Product.java
@@ -0,0 +1,16 @@
+public class Product {
+
+    int productId;
+    String productName;
+    String category;
+
+    public Product(int productId, String productName, String category) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+    }
+
+    public void display() {
+        System.out.println(productId + " " + productName + " " + category);
+    }
+}
