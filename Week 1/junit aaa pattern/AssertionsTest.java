ab11f6d2a95c7d6b365f93f937a79930698de461
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 16:42:26 +0530

    Create AssertionsTest.java

---
+++ Week 1/junit aaa pattern/AssertionsTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions() {
+
+        assertEquals(5, 2 + 3);
+
+        assertTrue(5 > 3);
+
+        assertFalse(5 < 3);
+
+        assertNull(null);
+
+        assertNotNull(new Object());
+    }
+}
