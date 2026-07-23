366d69436b6e98b11041be7d64dac42195f2b7c0
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 22:40:51 +0530

    Create Contact.js

---
+++ Week5/2. ReactJS-HOL/Contact.js
@@ -0,0 +1,9 @@
+function Contact() {
+    return (
+        <div>
+            <h2>Welcome to the Contact page of the Student Management Portal</h2>
+        </div>
+    );
+}
+
+export default Contact;
