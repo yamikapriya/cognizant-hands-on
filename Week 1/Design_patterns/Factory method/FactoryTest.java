3143c1efe1b558b0ed6fde2d57fc560ee0236f49
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 15:00:23 +0530

    Create FactoryTest.java

---
+++ Week 1/Design_patterns/Factory method/FactoryTest.java
@@ -0,0 +1,17 @@
+public class FactoryTest {
+
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document word = wordFactory.createDocument();
+        word.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdf = pdfFactory.createDocument();
+        pdf.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excel = excelFactory.createDocument();
+        excel.open();
+    }
+}
