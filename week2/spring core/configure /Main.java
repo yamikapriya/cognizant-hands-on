75a3d6a73e678b5f5461fbe4360dbd5e0d43539d
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 17:50:06 +0530

    Create Main.java

---
+++ week2/spring core/configure /Main.java
@@ -0,0 +1,11 @@
+package com.library;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("LibraryManagement Maven Project Configured Successfully!");
+
+    }
+
+}
