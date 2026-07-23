11dd45abfb65bf43c5f3de70b087125b43c1a67f
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 17:10:49 +0530

    Create Main.java

---
+++ Week 1/SL4J/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.error("This is an error message");
+
+        logger.warn("This is a warning message");
+
+    }
+}
