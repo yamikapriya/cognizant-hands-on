7c896346f5964c992be638137064308bf359896f
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 19:12:10 +0530

    Create Scenario1.sql

---
+++ Week 1/sql/stored procedures/Scenario1.sql
@@ -0,0 +1,12 @@
+-- Scenario 1: Process Monthly Interest
+
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+AS
+BEGIN
+    UPDATE Accounts
+    SET Balance = Balance + (Balance * 0.01)
+    WHERE AccountType = 'Savings';
+
+    COMMIT;
+END;
+/
