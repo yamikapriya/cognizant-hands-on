6ea18d31ff8084f253d91c906921cc70ee0b9dd5
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 16:42:11 +0530

    Create MainTest.java

---
+++ Week 1/junit aaa pattern/MainTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testAddition() {
+
+        assertEquals(5, 2 + 3);
+
+    }
+}
