54404799fbf1c56b506cf15c65df29113d5fb763
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 19:16:44 +0530

    Create Scenario3.sql

---
+++ Week 1/sql/stored procedures/Scenario3.sql
@@ -0,0 +1,33 @@
+-- Scenario 3: Transfer Funds
+
+CREATE OR REPLACE PROCEDURE TransferFunds
+(
+    p_FromAccount IN NUMBER,
+    p_ToAccount IN NUMBER,
+    p_Amount IN NUMBER
+)
+AS
+    v_Balance NUMBER;
+BEGIN
+    SELECT Balance
+    INTO v_Balance
+    FROM Accounts
+    WHERE AccountID = p_FromAccount;
+
+    IF v_Balance >= p_Amount THEN
+        UPDATE Accounts
+        SET Balance = Balance - p_Amount
+        WHERE AccountID = p_FromAccount;
+
+        UPDATE Accounts
+        SET Balance = Balance + p_Amount
+        WHERE AccountID = p_ToAccount;
+
+        COMMIT;
+
+        DBMS_OUTPUT.PUT_LINE('Transfer Successful');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Insufficient Balance');
+    END IF;
+END;
+/
