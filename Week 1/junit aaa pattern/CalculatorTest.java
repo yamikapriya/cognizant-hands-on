1626cf2c71d4bf611a4c9467d16479a1a5a281d1
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-23 16:42:41 +0530

    Create CalculatorTest.java

---
+++ Week 1/junit aaa pattern/CalculatorTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up...");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up...");
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
