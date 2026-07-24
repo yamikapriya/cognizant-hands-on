0af619cc5c6b35aa4aae1209ce1da5993738ca14
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-24 08:57:41 +0530

    Create SpringLearnApplication.java

---
+++ week3/1. spring-rest-handson/Spring Core – Load SimpleDateFormat from Spring Configuration XML./SpringLearnApplication.java
@@ -0,0 +1,36 @@
+package com.cognizant.springlearn;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(SpringLearnApplication.class, args);
+
+        displayDate();
+
+    }
+
+    public static void displayDate() throws Exception {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("date-format.xml");
+
+        SimpleDateFormat format =
+                context.getBean("dateFormat", SimpleDateFormat.class);
+
+        Date date = format.parse("31/12/2018");
+
+        System.out.println(date);
+
+    }
+
+}
