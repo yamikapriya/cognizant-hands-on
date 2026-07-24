b10c25eb334acc5e99f57ff5f5700bc8192caab2
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-24 09:52:40 +0530

    Create JwtRequestFilter.java

---
+++ week3/5. JWT-handson/Create authentication service that returns JWT/JwtRequestFilter.java
@@ -0,0 +1,25 @@
+package com.cognizant.springlearn.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+    }
+}
