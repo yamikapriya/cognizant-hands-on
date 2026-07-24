81695a9e8a770224146c20d63b23f12ec35b9bb1
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-24 09:43:01 +0530

    Create CountryController.java

---
+++ week3/2. spring-rest-handson/REST - Get country based on country code/CountryController.java
@@ -0,0 +1,22 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.springlearn.model.Country;
+import com.cognizant.springlearn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/country/{code}")
+    public Country getCountry(@PathVariable String code) {
+
+        return countryService.getCountry(code);
+    }
+}
