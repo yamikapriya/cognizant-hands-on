a76e3083ab11f6a32c81e3e47455fea658dd78bc
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-24 09:52:06 +0530

    Create UserDetailsServiceImpl.java

---
+++ week3/5. JWT-handson/Create authentication service that returns JWT/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.cognizant.springlearn.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        return new User(
+                "user",
+                "{noop}pwd",
+                new ArrayList<>());
+    }
+}
