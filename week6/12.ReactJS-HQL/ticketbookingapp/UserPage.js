7a35d114fd1b9cabf6069da92575ea5b1a6836dd
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-25 10:24:23 +0530

    Create UserPage.js

---
+++ week6/12.ReactJS-HQL/ticketbookingapp/UserPage.js
@@ -0,0 +1,20 @@
+function UserPage() {
+  return (
+    <div>
+      <h2>Welcome User</h2>
+
+      <h3>Flight Details</h3>
+
+      <ul>
+        <li>Flight : AI-202</li>
+        <li>From : Hyderabad</li>
+        <li>To : Delhi</li>
+        <li>Time : 10:30 AM</li>
+      </ul>
+
+      <button>Book Ticket</button>
+    </div>
+  );
+}
+
+export default UserPage;
