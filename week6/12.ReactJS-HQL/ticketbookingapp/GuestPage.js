a3a77ff16278ed2d3893be47f4bf681bc2789bf2
yamikapriya <yamikapriyachalla@gmail.com>
2026-07-25 10:24:03 +0530

    Create GuestPage.js

---
+++ week6/12.ReactJS-HQL/ticketbookingapp/GuestPage.js
@@ -0,0 +1,20 @@
+function GuestPage() {
+  return (
+    <div>
+      <h2>Welcome Guest</h2>
+
+      <h3>Flight Details</h3>
+
+      <ul>
+        <li>Flight : AI-202</li>
+        <li>From : Hyderabad</li>
+        <li>To : Delhi</li>
+        <li>Time : 10:30 AM</li>
+      </ul>
+
+      <p>Please Login to Book Tickets.</p>
+    </div>
+  );
+}
+
+export default GuestPage;
